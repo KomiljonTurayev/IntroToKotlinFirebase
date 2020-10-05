3ef39d253235d14688f6978fc4ce8cb4e4942cc7
Komiljon <turayevkomiljon@gmail.com>
2020-10-05 20:22:25 +0500

    Initial commit

---
+++ app/src/main/java/com/example/introtokotlinfirebase/MainActivity.kt
@@ -0,0 +1,19 @@
+ package com.example.introtokotlinfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+ class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val database:FirebaseDatabase = FirebaseDatabase.getInstance()
+        val myRef:DatabaseReference = database.reference
+
+        myRef.setValue("Hello, World!")
+
+    }
+}
